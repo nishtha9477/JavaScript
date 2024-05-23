5d9ac4eb095def93f638430f460b55bc864b1dd8
Nishtha Maurya <108268302+nishtha9477@users.noreply.github.com>
2024-05-24 01:45:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+console.log("hello ABC");
+
+// // VARIABLE
+// 1-LET
+let a= 5; 
+console.log(a);
+let name= 'nishtha';
+console.log(name);
+
+//2- VAR
+var c =14.5;
+console.log(c);
+
+// scope of let and var
+if(true){
+  let x=4;
+  console.log(x);
+}
+
+if (true){
+  var y=12.59;
+  console.log(y);
+}
+console.log(y);
+
+// CONSTANT
+const num=12;
+// num=22; 
+// not assign another value to a constant
+
+// DYNAMIC TYPING 
+let lastName= 'Nishtha';
+console.log(lastName);
+
+lastName= 122;
+console.log(lastName);
+
+// DATA TYPES
+ let n=2; 
+ n= 'nishtha';
+ n= true; 
+ 
+//  array
+let names=['ankur', 'raj', 'shalini'];
+console.log(names[1]);
+names[2]=3;
+console.log(names[2]);
+names[3]='puja';
+console.log(names[3]);
